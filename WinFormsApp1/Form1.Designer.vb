38ca59dab6d152d72b84a542593d3f7a68f712f1
railei <raileidugayo8220@gmail.com>
2025-02-09 15:09:32 +0800

    ver1

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TXTBOX_DIGITSCREEN = New RichTextBox()
+        BTN1 = New Button()
+        BTN2 = New Button()
+        BTN3 = New Button()
+        BTN4 = New Button()
+        BTN5 = New Button()
+        BTN6 = New Button()
+        BTN7 = New Button()
+        BTN8 = New Button()
+        BTN9 = New Button()
+        BTN0 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        BTN_DECIMAL = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Label2 = New Label()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TXTBOX_DIGITSCREEN
+        ' 
+        TXTBOX_DIGITSCREEN.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        TXTBOX_DIGITSCREEN.Font = New Font("Showcard Gothic", 26.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TXTBOX_DIGITSCREEN.Location = New Point(59, 105)
+        TXTBOX_DIGITSCREEN.Name = "TXTBOX_DIGITSCREEN"
+        TXTBOX_DIGITSCREEN.ReadOnly = True
+        TXTBOX_DIGITSCREEN.ScrollBars = RichTextBoxScrollBars.None
+        TXTBOX_DIGITSCREEN.Size = New Size(249, 50)
+        TXTBOX_DIGITSCREEN.TabIndex = 0
+        TXTBOX_DIGITSCREEN.Text = ""
+        ' 
+        ' BTN1
+        ' 
+        BTN1.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN1.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN1.ForeColor = Color.FloralWhite
+        BTN1.Location = New Point(24, 198)
+        BTN1.Name = "BTN1"
+        BTN1.Size = New Size(51, 29)
+        BTN1.TabIndex = 1
+        BTN1.Text = "1"
+        BTN1.UseVisualStyleBackColor = False
+        ' 
+        ' BTN2
+        ' 
+        BTN2.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN2.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN2.ForeColor = Color.FloralWhite
+        BTN2.Location = New Point(81, 198)
+        BTN2.Name = "BTN2"
+        BTN2.Size = New Size(51, 29)
+        BTN2.TabIndex = 2
+        BTN2.Text = "2"
+        BTN2.UseVisualStyleBackColor = False
+        ' 
+        ' BTN3
+        ' 
+        BTN3.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN3.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN3.ForeColor = Color.FloralWhite
+        BTN3.Location = New Point(138, 198)
+        BTN3.Name = "BTN3"
+        BTN3.Size = New Size(51, 29)
+        BTN3.TabIndex = 3
+        BTN3.Text = "3"
+        BTN3.UseVisualStyleBackColor = False
+        ' 
+        ' BTN4
+        ' 
+        BTN4.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN4.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN4.ForeColor = Color.FloralWhite
+        BTN4.Location = New Point(24, 246)
+        BTN4.Name = "BTN4"
+        BTN4.Size = New Size(51, 29)
+        BTN4.TabIndex = 4
+        BTN4.Text = "4"
+        BTN4.UseVisualStyleBackColor = False
+        ' 
+        ' BTN5
+        ' 
+        BTN5.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN5.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN5.ForeColor = Color.FloralWhite
+        BTN5.Location = New Point(81, 246)
+        BTN5.Name = "BTN5"
+        BTN5.Size = New Size(51, 29)
+        BTN5.TabIndex = 5
+        BTN5.Text = "5"
+        BTN5.UseVisualStyleBackColor = False
+        ' 
+        ' BTN6
+        ' 
+        BTN6.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN6.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN6.ForeColor = Color.FloralWhite
+        BTN6.Location = New Point(138, 246)
+        BTN6.Name = "BTN6"
+        BTN6.Size = New Size(51, 29)
+        BTN6.TabIndex = 6
+        BTN6.Text = "6"
+        BTN6.UseVisualStyleBackColor = False
+        ' 
+        ' BTN7
+        ' 
+        BTN7.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN7.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN7.ForeColor = Color.FloralWhite
+        BTN7.Location = New Point(24, 299)
+        BTN7.Name = "BTN7"
+        BTN7.Size = New Size(51, 29)
+        BTN7.TabIndex = 7
+        BTN7.Text = "7"
+        BTN7.UseVisualStyleBackColor = False
+        ' 
+        ' BTN8
+        ' 
+        BTN8.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN8.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN8.ForeColor = Color.FloralWhite
+        BTN8.Location = New Point(81, 299)
+        BTN8.Name = "BTN8"
+        BTN8.Size = New Size(51, 29)
+        BTN8.TabIndex = 8
+        BTN8.Text = "8"
+        BTN8.UseVisualStyleBackColor = False
+        ' 
+        ' BTN9
+        ' 
+        BTN9.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN9.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN9.ForeColor = Color.FloralWhite
+        BTN9.Location = New Point(138, 299)
+        BTN9.Name = "BTN9"
+        BTN9.Size = New Size(51, 29)
+        BTN9.TabIndex = 9
+        BTN9.Text = "9"
+        BTN9.UseVisualStyleBackColor = False
+        ' 
+        ' BTN0
+        ' 
+        BTN0.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN0.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN0.ForeColor = Color.FloralWhite
+        BTN0.Location = New Point(81, 349)
+        BTN0.Name = "BTN0"
+        BTN0.Size = New Size(108, 27)
+        BTN0.TabIndex = 10
+        BTN0.Text = "0"
+        BTN0.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.FromArgb(CByte(58), CByte(90), CByte(64))
+        Button11.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button11.ForeColor = Color.FloralWhite
+        Button11.Location = New Point(207, 198)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(51, 38)
+        Button11.TabIndex = 11
+        Button11.Text = "+"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.FromArgb(CByte(58), CByte(90), CByte(64))
+        Button12.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button12.ForeColor = Color.FloralWhite
+        Button12.Location = New Point(207, 246)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(51, 38)
+        Button12.TabIndex = 12
+        Button12.Text = "-"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.FromArgb(CByte(58), CByte(90), CByte(64))
+        Button13.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button13.ForeColor = Color.FloralWhite
+        Button13.Location = New Point(207, 299)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(51, 38)
+        Button13.TabIndex = 13
+        Button13.Text = "*"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.FromArgb(CByte(58), CByte(90), CByte(64))
+        Button14.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button14.ForeColor = Color.FloralWhite
+        Button14.Location = New Point(207, 349)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(51, 38)
+        Button14.TabIndex = 14
+        Button14.Text = "/"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' BTN_DECIMAL
+        ' 
+        BTN_DECIMAL.BackColor = Color.FromArgb(CByte(69), CByte(80), CByte(59))
+        BTN_DECIMAL.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BTN_DECIMAL.ForeColor = Color.FloralWhite
+        BTN_DECIMAL.Location = New Point(24, 349)
+        BTN_DECIMAL.Name = "BTN_DECIMAL"
+        BTN_DECIMAL.Size = New Size(51, 27)
+        BTN_DECIMAL.TabIndex = 15
+        BTN_DECIMAL.Text = "."
+        BTN_DECIMAL.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.FromArgb(CByte(58), CByte(90), CByte(64))
+        Button16.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button16.ForeColor = Color.FloralWhite
+        Button16.Location = New Point(279, 299)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(51, 89)
+        Button16.TabIndex = 16
+        Button16.Text = "="
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = Color.FromArgb(CByte(27), CByte(58), CByte(26))
+        Button17.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button17.ForeColor = Color.FloralWhite
+        Button17.Location = New Point(279, 198)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(51, 38)
+        Button17.TabIndex = 17
+        Button17.Text = "c"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = Color.FromArgb(CByte(27), CByte(58), CByte(26))
+        Button18.Font = New Font("SimSun", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button18.ForeColor = Color.FloralWhite
+        Button18.Location = New Point(279, 246)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(51, 38)
+        Button18.TabIndex = 18
+        Button18.Text = "ce"
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.Location = New Point(91, 38)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(188, 33)
+        Label2.TabIndex = 20
+        Label2.Text = "CALCULATOR"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label1
+        ' 
+        Label1.Location = New Point(108, 443)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(188, 10)
+        Label1.TabIndex = 21
+        Label1.Text = "CALCULATOR"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(108), CByte(124), CByte(89))
+        ClientSize = New Size(364, 503)
+        Controls.Add(Label1)
+        Controls.Add(Label2)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(BTN_DECIMAL)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(BTN0)
+        Controls.Add(BTN9)
+        Controls.Add(BTN8)
+        Controls.Add(BTN7)
+        Controls.Add(BTN6)
+        Controls.Add(BTN5)
+        Controls.Add(BTN4)
+        Controls.Add(BTN3)
+        Controls.Add(BTN2)
+        Controls.Add(BTN1)
+        Controls.Add(TXTBOX_DIGITSCREEN)
+        ForeColor = Color.DimGray
+        MaximumSize = New Size(380, 542)
+        MinimumSize = New Size(380, 542)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = " "
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TXTBOX_DIGITSCREEN As RichTextBox
+    Friend WithEvents BTN1 As Button
+    Friend WithEvents BTN2 As Button
+    Friend WithEvents BTN3 As Button
+    Friend WithEvents BTN4 As Button
+    Friend WithEvents BTN5 As Button
+    Friend WithEvents BTN6 As Button
+    Friend WithEvents BTN7 As Button
+    Friend WithEvents BTN8 As Button
+    Friend WithEvents BTN9 As Button
+    Friend WithEvents BTN0 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents BTN_DECIMAL As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+
+End Class
